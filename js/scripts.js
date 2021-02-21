cf60b4a3ac7f4b43185a140f59c38d914569bb3b
shalom333-cpu <elishaolimbas@gmail.com>
2021-02-21 13:52:37 +0300

    add if else statement

--- js/scripts.js
+++ js/scripts.js
@@ -34,4 +34,25 @@ form.addEventListener('submit',(event) => {
 
   let genderExtract = form.elements['gender'];
   let userGender = genderExtract.value; 
-}
+
+  if (dateOfBirth && userGender) {
+     
+    dayNumber = giveDayNumberofDate(dateOfBirth);
+     
+      if (dayNumber >= 0 && dayNumber <= 6) {
+          
+        assignedName = givePersonName(userGender);
+
+        if (assignedName){ 
+          alert("Your Akan name is: " + assignedName);
+        } else {
+          alert("An error occured while assigning the name. Please try again");
+        }
+
+      }else{
+        alert("The day you've entered does not exist. Please select a valid date");
+      }
+  }else{
+    alert("Please ensure you've submitted all the details");
+  }
+});
