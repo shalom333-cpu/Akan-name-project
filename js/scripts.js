27dab9ee85c036be07e80d9d53f41866c8e84965
shalom333-cpu <elishaolimbas@gmail.com>
2021-02-21 12:34:19 +0300

    add first function

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@ let form = document.getElementById('name_form');
 
 let maleNameArrays = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femalNameArrays = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function giveDayNumberofDate(val1){
+  var number = "";
+  var dt = new Date(val1)
+  number = dt.getDay();
+  return number;
+}
