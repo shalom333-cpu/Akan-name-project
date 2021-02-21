915a7fad6da0d04079b2a3df69f50f867408f7b4
shalom333-cpu <elishaolimbas@gmail.com>
2021-02-21 12:25:58 +0300

    add arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,4 @@
 let form = document.getElementById('name_form');
+
+let maleNameArrays = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femalNameArrays = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
