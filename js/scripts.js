253c1c539d58d561db54dd865676fd15715659a8
shalom333-cpu <elishaolimbas@gmail.com>
2021-02-21 14:27:22 +0300

    edit functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,34 @@ let form = document.getElementById('name_form');
 let maleNameArrays = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femalNameArrays = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-function giveDayNumberofDate(val1){
+
+
+function giveDayNumberofDate(val1) {
+  /*initialize a blank variable*/
   var number = "";
-  var dt = new Date(val1)
+
+  var dt = new Date(val1);
+  /*get day Number*/
   number = dt.getDay();
   return number;
 }
 
-function givePersonName(val2){
+
+function givePersonName(val2) {
 
   var producedName = "";
-  switch(val2){
+  switch (val2) {
     case 'Female':
       producedName = femalNameArrays[dayNumber];
       break;
 
     case 'Male':
       producedName = maleNameArrays[dayNumber];
-      break; 
+      break;
   }
-  
+
   return producedName;
+
 }
 
 form.addEventListener('submit', (event) => {
